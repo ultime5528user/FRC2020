2cb2633977ffa02d68b098d4e9ab69dd2735212e
Ultime 5528 <keranna.first@gmail.com>
2020-01-31 14:14:17 -0500

    Modification noms CubicInterpolator

--- src/main/java/com/ultime5528/util/CubicInterpolator.java
+++ src/main/java/com/ultime5528/util/CubicInterpolator.java
@@ -2,36 +2,36 @@ package com.ultime5528.util;
 
 public class CubicInterpolator {
 
-	private double courbure, deadzoneVitesse, deadzoneJoystick;
+	private double courbure, deadzoneY, deadzoneX;
 
 	/**
 	 * 
-	 * @param courbure         Définit le niveau de courbure. 0 : DROIT => 1 : COURBE MAXIMALE
-	 * @param deadzoneVitesse  Définit le niveau minimal de vitesse. 
-	 * @param deadzoneJoystick Définit le niveau minimal de poussée sur le joystick.
+	 * @param courbure  Définit le niveau de courbure. 0 : DROIT => 1 : COURBE MAXIMALE
+	 * @param deadzoneY Définit le niveau minimal en Y (habituellement: vitesse). 
+	 * @param deadzoneX Définit le niveau minimal X (habituellement: poussée sur le joystick).
 	 */
-	public CubicInterpolator(double courbure, double deadzoneVitesse, double deadzoneJoystick) {
+	public CubicInterpolator(double courbure, double deadzoneY, double deadzoneX) {
 
 		this.courbure = courbure;
-		this.deadzoneVitesse = deadzoneVitesse;
-		this.deadzoneJoystick = deadzoneJoystick;
+		this.deadzoneY = deadzoneY;
+		this.deadzoneX = deadzoneX;
 	}
 
 	public double interpolate(double valeur) {
 
-		if (valeur >= deadzoneJoystick) {
+		if (valeur >= deadzoneX) {
 
-			return deadzoneVitesse
-					+ (1 - deadzoneVitesse) * (courbure * valeur * valeur * valeur + (1 - courbure) * valeur);
+			return deadzoneY
+					+ (1 - deadzoneY) * (courbure * valeur * valeur * valeur + (1 - courbure) * valeur);
 
-		} else if (valeur <= -deadzoneJoystick) {
+		} else if (valeur <= -deadzoneX) {
 
-			return -deadzoneVitesse
-					+ (1 - deadzoneVitesse) * (courbure * valeur * valeur * valeur + (1 - courbure) * valeur);
+			return -deadzoneY
+					+ (1 - deadzoneY) * (courbure * valeur * valeur * valeur + (1 - courbure) * valeur);
 
 		} else {
 
-			return interpolate(deadzoneJoystick) / deadzoneJoystick * valeur;
+			return interpolate(deadzoneX) / deadzoneX * valeur;
 
 		}
 	}
@@ -46,19 +46,19 @@ public class CubicInterpolator {
 	}
 
 	public double getDeadzoneVitesse() {
-		return deadzoneVitesse;
+		return deadzoneY;
 	}
 
 	public void setDeadzoneVitesse(double deadzoneVitesse) {
-		this.deadzoneVitesse = deadzoneVitesse;
+		this.deadzoneY = deadzoneVitesse;
 	}
 
 	public double getDeadzoneJoystick() {
-		return deadzoneJoystick;
+		return deadzoneX;
 	}
 
 	public void setDeadzoneJoystick(double deadzoneJoystick) {
-		this.deadzoneJoystick = deadzoneJoystick;
+		this.deadzoneX = deadzoneJoystick;
 	}
 
 }
