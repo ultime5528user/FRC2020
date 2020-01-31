bd99e78b39abb2a2d5fa7108533e07d025a4b504
Ultime 5528 <keranna.first@gmail.com>
2020-01-31 16:02:17 -0500

    Ajout capteur couleur roulette

--- src/main/java/com/ultime5528/frc2020/subsystems/Roulette.java
+++ src/main/java/com/ultime5528/frc2020/subsystems/Roulette.java
@@ -7,9 +7,14 @@
 
 package com.ultime5528.frc2020.subsystems;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.LinearFilter;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.ColorSensorV3;
 import com.ultime5528.frc2020.Constants;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Config;
@@ -22,14 +27,21 @@ public class Roulette extends SubsystemBase implements Loggable {
   public static double kTempsTourner = 10;
   private VictorSP moteurRoulette;
 
+  private ColorSensorV3 capteurCouleur;
+  @Config
+  private final I2C.Port kCapteurCouleurPort = I2C.Port.kOnboard;
+
   public Roulette() {
     moteurRoulette = new VictorSP(Constants.Ports.ROULETTE_MOTEUR);
+    capteurCouleur = new ColorSensorV3(kCapteurCouleurPort);
     SendableRegistry.addLW(moteurRoulette, getSubsystem(), "Moteur Roulette");
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("R Roulette", capteurCouleur.getRed());
+    SmartDashboard.putNumber("G Roulette", capteurCouleur.getGreen());
+    SmartDashboard.putNumber("B Roulette", capteurCouleur.getBlue());
   }
 
   public void tourner() {
