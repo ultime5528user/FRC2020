27976589bfbaae003f098ec7400a1460c9152111
Ultime 5528 <keranna.first@gmail.com>
2020-02-01 15:09:21 -0500

    CubicInterpolator sur joystick

--- src/main/java/com/ultime5528/frc2020/commands/Piloter.java
+++ src/main/java/com/ultime5528/frc2020/commands/Piloter.java
@@ -9,30 +9,52 @@ package com.ultime5528.frc2020.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.annotations.Config;
+
 import com.ultime5528.frc2020.subsystems.BasePilotable;
+import com.ultime5528.util.CubicInterpolator;
 
-public class Piloter extends CommandBase {
+public class Piloter extends CommandBase implements Loggable {
 
-  BasePilotable basePilotable;
-  Joystick joystick;
+  private BasePilotable basePilotable;
+  private Joystick joystick;
+
+  public static double kCourbureX = 0;
+  public static double kDeadzoneJoystickX = 0.5;
+  public static double kDeadzoneVitesseX = 0.5;
+
+  public static double kCourbureY = 0;
+  public static double kDeadzoneJoystickY = 0.5;
+  public static double kDeadzoneVitesseY = 0.5;
+
+  @Config.NumberSlider(name = "Courbure X", min = 0, max = 1, methodName = "setCourbure")
+  @Config.NumberSlider(name = "Deadzone Joystick X", min = 0, max = 1, methodName = "setDeadzoneX")
+  @Config.NumberSlider(name = "Deadzone Vitesse X", min = 0, max = 1, methodName = "setDeadzoneY")
+  private CubicInterpolator cubicInterpolatorX = new CubicInterpolator(kCourbureX, kDeadzoneJoystickX,
+      kDeadzoneVitesseX);
+
+  @Config.NumberSlider(name = "Courbure Y", min = 0, max = 1, methodName = "setCourbure")
+  @Config.NumberSlider(name = "Deadzone Joystick Y", min = 0, max = 1, methodName = "setDeadzoneX")
+  @Config.NumberSlider(name = "Deadzone Vitesse Y", min = 0, max = 1, methodName = "setDeadzoneY")
+  private CubicInterpolator cubicInterpolatorY = new CubicInterpolator(kCourbureY, kDeadzoneJoystickY,
+      kDeadzoneVitesseY);
+
   public Piloter(Joystick joystick, BasePilotable basePilotable) {
     addRequirements(basePilotable);
     this.basePilotable = basePilotable;
     this.joystick = joystick;
-
   }
-
 
   @Override
   public void initialize() {
   }
 
-
   @Override
   public void execute() {
-    basePilotable.drive(joystick.getY(), joystick.getX());
+    basePilotable.drive(cubicInterpolatorY.interpolate(joystick.getY()),
+        cubicInterpolatorX.interpolate(joystick.getX()));
   }
-
 
   @Override
   public void end(boolean interrupted) {
