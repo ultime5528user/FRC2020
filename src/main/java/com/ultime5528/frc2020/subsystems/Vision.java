06f95e0b041af408cd58cc0c58cf2a45347671b5
Ultime 5528 <keranna.first@gmail.com>
2020-02-04 18:36:06 -0500

    Ajout VisionSnapshot
    implémentation en cours

--- src/main/java/com/ultime5528/frc2020/subsystems/Vision.java
+++ src/main/java/com/ultime5528/frc2020/subsystems/Vision.java
@@ -7,45 +7,51 @@
 
 package com.ultime5528.frc2020.subsystems;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalDouble;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision {
 
-  private NetworkTableEntry xEntry;
-  private NetworkTableEntry largeurEntry;
-  private NetworkTableEntry foundEntry;
+  private NetworkTableEntry snapshotEntry;
 
   private static Vision instance;
 
-  private static double kFOV = 49.8; //Degrés
-  private static double kFocale = 1/Math.tan(Math.toRadians(kFOV)/2);
+  private static double kFOV = 49.8; // Degrés
+  private static double kFocale = 1 / Math.tan(Math.toRadians(kFOV) / 2);
+
+  private static VisionSnapshot currentSnapshot;
 
   private Vision() {
-    xEntry = NetworkTableInstance.getDefault().getTable("Vision").getEntry("xcible");
-    largeurEntry = NetworkTableInstance.getDefault().getTable("Vision").getEntry("largeurcible");
-    foundEntry = NetworkTableInstance.getDefault().getTable("Vision").getEntry("found");
+    snapshotEntry = NetworkTableInstance.getDefault().getTable("Vision").getEntry("Snapshot");
   }
 
-  public static Vision getInstance(){
-    if(instance == null)
+  public static Vision getInstance() {
+    if (instance == null)
       instance = new Vision();
+    instance.readSnapshot();
     return instance;
   }
 
-  public static OptionalDouble getLargeurCible(){
-    if(getInstance().foundEntry.getBoolean(false)){
-      return OptionalDouble.of(getInstance().largeurEntry.getDouble(0.0));
+  public static void readSnapshot(){
+    currentSnapshot = VisionSnapshot.from(snapshotEntry.getString());
+  }
+
+  public static OptionalDouble getLargeurCible() {
+    if (getInstance().currentSnapshot.found) {
+      return OptionalDouble.of(getInstance().currentSnapshot.largeur);
     } else {
       return OptionalDouble.empty();
     }
   }
 
-  public static OptionalDouble getAngleCible(){
-    if(getInstance().foundEntry.getBoolean(false)){
-      double x = getInstance().xEntry.getDouble(0);
+  public static OptionalDouble getAngleCible() {
+    if (getInstance().currentSnapshot.found) {
+      double x = getInstance().currentSnapshot.xpos;
       double angle = Math.atan(kFocale / x);
 
       return OptionalDouble.of(angle);
@@ -53,4 +59,30 @@ public class Vision {
       return OptionalDouble.empty();
     }
   }
+
+  public static class VisionSnapshot {
+
+    public final long timestamp;
+
+    public final boolean found;
+    public final double xpos;
+    public final double largeur;
+
+    public VisionSnapshot(long timestamp, boolean found, double xpos, double largeur) {
+      this.timestamp = timestamp;
+
+      this.found = found;
+      this.xpos = xpos;
+      this.largeur = largeur;
+    }
+
+    public static VisionSnapshot from(String fromString) {
+      List<String> splitted = Arrays.asList(fromString.split(";"));
+
+      return new VisionSnapshot(Long.parseLong(splitted.get(0)), Integer.parseInt(splitted.get(1)) == 1,
+          Double.parseDouble(splitted.get(2)), Double.parseDouble(splitted.get(3)));
+    }
+
+  }
+
 }
