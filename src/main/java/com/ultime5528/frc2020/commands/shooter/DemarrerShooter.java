318508727a60c8c31e1537fa75b6e2ed4d4d0877
Ultime 5528 <keranna.first@gmail.com>
2020-02-29 16:37:31 -0500

    Ajout DemarrerShooter

---
+++ src/main/java/com/ultime5528/frc2020/commands/shooter/DemarrerShooter.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.ultime5528.frc2020.commands.shooter;
+
+import java.util.OptionalDouble;
+
+import com.ultime5528.frc2020.subsystems.Intake;
+import com.ultime5528.frc2020.subsystems.Shooter;
+import com.ultime5528.frc2020.subsystems.VisionController;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.ultime5528.util.Timer;
+
+public class DemarrerShooter extends CommandBase {
+
+  private Shooter shooter;
+  private VisionController vision;
+
+  public DemarrerShooter(Shooter shooter, VisionController vision) {
+    this.shooter = shooter;
+   
+    this.vision = vision;
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    vision.enable();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    shooter.tirer(vision.getHauteurCible());
+    //shooter.tirer(OptionalDouble.empty()); // TODO Vitesse de la dernière cible vue, dans ajouter une variable qui la contient
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
