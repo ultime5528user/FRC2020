017d9fec435cf9f666cef11d676db9f3bbc02b82
ZedeV2 <tremblayalexis@outlook.com>
2020-03-01 15:30:04 -0500

    Utilisation du lag pur plus de précision

--- src/main/java/com/ultime5528/frc2020/subsystems/VisionController.java
+++ src/main/java/com/ultime5528/frc2020/subsystems/VisionController.java
@@ -24,6 +24,7 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Relay.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -46,7 +47,7 @@ public class VisionController extends SubsystemBase {
   private boolean isEnabled = true; // TODO remettre à false ?
   private boolean hasSynchronized = false;
   private long doSynchronizeTime, lag;
- 
+
   private Supplier<Long> timestampSupplier;
 
   public VisionController(Supplier<Long> timestampSupplier) {
@@ -54,21 +55,21 @@ public class VisionController extends SubsystemBase {
     var visionTable = NetworkTableInstance.getDefault().getTable("vision");
 
     startVisionEntry = visionTable.getEntry("start_vision");
-    
+
     snapshotEntry = visionTable.getEntry("snapshot");
     timestampEntry = visionTable.getEntry("timestamp");
     doSynchronizeEntry = visionTable.getEntry("do_synchronize");
     gotSynchronizeEntry = visionTable.getEntry("got_synchronize");
 
     this.timestampSupplier = timestampSupplier;
-    
+
     raspberryPiStartedEntry = visionTable.getEntry("pi_started");
     raspberryPiStartedEntry.addListener(notif -> {
       if (notif.value.getBoolean()) {
         synchronize();
       }
     }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate | EntryListenerFlags.kImmediate);
-    
+
     startVisionEntry.setBoolean(true);
 
     led = new Relay(Ports.VISION_LED);
@@ -85,7 +86,7 @@ public class VisionController extends SubsystemBase {
 
     CvSource inverseSrc = CameraServer.getInstance().putVideo("Inverse", 480, 360);
 
-    Thread visionThread = new Thread(() -> {  
+    Thread visionThread = new Thread(() -> {
       Mat frame = new Mat();
       CvSink sink = new CvSink("cam_sink");
       sink.setSource(cam);
@@ -96,7 +97,8 @@ public class VisionController extends SubsystemBase {
             Core.flip(frame, frame, -1);
             inverseSrc.putFrame(frame);
           }
-        } catch (Exception e) { }
+        } catch (Exception e) {
+        }
       }
       sink.close();
     });
@@ -108,7 +110,7 @@ public class VisionController extends SubsystemBase {
 
   @Override
   public void periodic() {
-    led.set(Value.kOn); //TODO ENLEVERRRRR!
+    led.set(Value.kOn); // TODO ENLEVERRRRR!
 
     if (isEnabled) {
       readSnapshot();
@@ -142,7 +144,7 @@ public class VisionController extends SubsystemBase {
     led.set(Value.kOff);
   }
 
-  public void readSnapshot(){
+  public void readSnapshot() {
     String data = snapshotEntry.getString("0;false;0;0");
     currentSnapshot = VisionSnapshot.fromRasperryPiData(data);
   }
@@ -155,14 +157,14 @@ public class VisionController extends SubsystemBase {
     }
   }
 
-  public long getLastTimestamp(){
-    return currentSnapshot.timestamp;
+  public long getLastTimestamp() {
+    return currentSnapshot.timestamp + lag + (long)(TimedRobot.kDefaultPeriod / 2 * 1000);
   }
 
   public OptionalDouble getAngleCible() {
     if (currentSnapshot.found) {
       double x = currentSnapshot.centreX;
-      double angle = Math.atan(-x / kFocale); //TODO vérifier "-x / kFocale"
+      double angle = Math.atan(-x / kFocale); // TODO vérifier "-x / kFocale"
       angle = Math.toDegrees(angle);
       return OptionalDouble.of(angle);
     } else {
@@ -189,8 +191,8 @@ public class VisionController extends SubsystemBase {
     }
 
     /**
-     * Convertis en VisionSnapshot une chaîne de caractères sous
-     * le format "timestamp<long>;found<boolean>;centreX<double>;hauteur<double>"
+     * Convertis en VisionSnapshot une chaîne de caractères sous le format
+     * "timestamp<long>;found<boolean>;centreX<double>;hauteur<double>"
      */
     public static VisionSnapshot fromRasperryPiData(String data) {
 
@@ -199,11 +201,8 @@ public class VisionController extends SubsystemBase {
       try {
         String[] splitted = data.split(";");
 
-        snapshot = new VisionSnapshot(
-            Long.parseLong(splitted[0]),
-            Boolean.parseBoolean(splitted[1]),
-            Double.parseDouble(splitted[2]),
-            Double.parseDouble(splitted[3]));
+        snapshot = new VisionSnapshot(Long.parseLong(splitted[0]), Boolean.parseBoolean(splitted[1]),
+            Double.parseDouble(splitted[2]), Double.parseDouble(splitted[3]));
 
       } catch (Exception e) {
         DriverStation.reportError("Erreur de formatage dans fromRasperryPiData() : " + data, false);
